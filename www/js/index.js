555cfea627e7b27ea9755ee64c59d1acc969c887
Alex Rojas <jose.rojas@empowerlabs.com>
2015-03-30 03:05:46 -0600

    Se añadió sección de detalles en perfil

--- www/js/index.js
+++ www/js/index.js
@@ -33,9 +33,12 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+    	alert("listo");
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+    	alert("listo"+id);
     }
 };
+app.initialize();
