5650e2d2ff6ff9f2ce80630eb8b99c9fd54ffe84
Alex Rojas <jose.rojas@empowerlabs.com>
2015-04-07 18:59:50 -0500

    testing

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,5 @@
+
+var misDatos;
 var logged="ok";
 var todos= {};
 var todosCall={};
@@ -11,7 +13,41 @@ var fecha="no";
 $(document).ready(function () {
 	
 	
-	
+	module.controller('DetailController2', function($scope) {
+    //$scope.ons.notification.alert({message: ""+misDatos.url,title: "intellibanks"});
+    $scope.url = misDatos.url;
+  });
+
+  module.controller('MasterController2', function($scope, $data,$http) {
+    $scope.item = $data.item;  
+    $http.get('http://empowerlabs.com/proyectos/trackersAPI/mblocs/todos.php').
+    success(function(data, status, headers, config) {
+  	//$scope.ons.notification.alert({message: ""+data.url,title: "intellibanks"});
+   misDatos=data;
+   $scope.item=misDatos;
+    $scope.video=function(item){
+    	//$scope.ons.notification.alert({message: ""+item.url,title: "intellibanks"});
+    	$scope.ons.navigator.pushPage('page2.html', {title : "1"});}; 
+    $scope.audio=function(){
+    	$scope.ons.navigator.pushPage('page3.html', {title : "1"});}; 
+    $scope.texto=function(){
+    	$scope.ons.navigator.pushPage('page4.html', {title : "1"});};
+    $scope.presentacion=function(){
+    	$scope.ons.navigator.pushPage('page5.html', {title : "1"});};
+  }).
+  error(function(data, status, headers, config) {
+  	
+  });
+    
+  });
+
+  module.factory('$data', function() {
+      var data = {};
+      
+      data.item = [misDatos];
+      
+      return data;
+  });
 	
   module.controller('AppController', function($scope, $data) {
     $scope.doSomething = function() {
